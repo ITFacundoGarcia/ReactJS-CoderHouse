3056e0d1af8e2ad136c6c0a44523b46241318e3f
ITfacundoGarcia <itfacundo>
2021-09-03 03:11:39 -0300

    Se agrega maqueta de Cards al proyecto

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar/NavBar"
 import ItemListContainer from "./components/ItemListContainer/ItemListcontainer"
+import Cards from "./components/Cards/Cards"
 
 function App() {
   return (
     <>
   <NavBar/>
   <ItemListContainer saludo = {"Hola! Este es el espacio donde se encontrarán nuestros productos!"}/>
+  <Cards/>
   </>
   );
 }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+
+const Cards = () => {
+    return (
+        <div>
+            <ul style={{display: "flex"}}>
+                <li className="p-2">
+                    <Card style={{backgroundColor:"Grey", width: '18rem' }}>
+                        <Card.Img variant="top" src="https://www.cellshop.com/1565211-thickbox_default/whisky-white-horse-8-anos-1l.jpg" />
+                        <Card.Body className="text-center">
+                            <Card.Title>*WHITE HORSE 8 AÑOS*</Card.Title>
+                            <Card.Text>
+                                Tipo: Blended<hr/>
+                                Edad: 8 años<hr/>
+                                Volumen: 1000cm3<hr/>
+                            </Card.Text>
+                            <Button variant="primary">Agregar al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </li>
+                <li className="p-2">
+                    <Card style={{backgroundColor:"Grey", width: '18rem' }}>
+                        <Card.Img variant="top" src="https://www.cellshop.com/1565211-thickbox_default/whisky-white-horse-8-anos-1l.jpg" />
+                        <Card.Body className="text-center">
+                            <Card.Title>*WHITE HORSE 8 AÑOS*</Card.Title>
+                            <Card.Text>
+                                Tipo: Blended<hr/>
+                                Edad: 8 años<hr/>
+                                Volumen: 1000cm3<hr/>
+                            </Card.Text>
+                            <Button variant="primary">Agregar al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </li>
+                <li className="p-2">
+                    <Card style={{backgroundColor:"Grey", width: '18rem' }}>
+                        <Card.Img variant="top" src="https://www.cellshop.com/1565211-thickbox_default/whisky-white-horse-8-anos-1l.jpg" />
+                        <Card.Body className="text-center">
+                            <Card.Title>*WHITE HORSE 8 AÑOS*</Card.Title>
+                            <Card.Text>
+                                Tipo: Blended<hr/>
+                                Edad: 8 años<hr/>
+                                Volumen: 1000cm3<hr/>
+                            </Card.Text>
+                            <Button variant="primary">Agregar al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </li>
+                <li className="p-2">
+                    <Card style={{backgroundColor:"Grey", width: '18rem' }}>
+                        <Card.Img variant="top" src="https://www.cellshop.com/1565211-thickbox_default/whisky-white-horse-8-anos-1l.jpg" />
+                        <Card.Body className="text-center">
+                            <Card.Title>*WHITE HORSE 8 AÑOS*</Card.Title>
+                            <Card.Text>
+                                Tipo: Blended<hr/>
+                                Edad: 8 años<hr/>
+                                Volumen: 1000cm3<hr/>
+                            </Card.Text>
+                            <Button variant="primary">Agregar al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </li>
+                <li className="p-2">
+                    <Card style={{backgroundColor:"Grey", width: '18rem' }}>
+                        <Card.Img variant="top" src="https://www.cellshop.com/1565211-thickbox_default/whisky-white-horse-8-anos-1l.jpg" />
+                        <Card.Body className="text-center">
+                            <Card.Title>*WHITE HORSE 8 AÑOS*</Card.Title>
+                            <Card.Text>
+                                Tipo: Blended<hr/>
+                                Edad: 8 años<hr/>
+                                Volumen: 1000cm3<hr/>
+                            </Card.Text>
+                            <Button variant="primary">Agregar al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Cards
